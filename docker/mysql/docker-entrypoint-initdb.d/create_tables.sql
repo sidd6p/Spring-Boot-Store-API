fe2cb8c625390b690fbf2ed769e6e3bee23a3929
Siddhartha <siddpurwar@gmail.com>
2025-07-03 21:55:13 +0530

    BUILD: database tables in script

--- docker/mysql/docker-entrypoint-initdb.d/create_tables.sql
+++ docker/mysql/docker-entrypoint-initdb.d/create_tables.sql
@@ -1,23 +1,53 @@
-create table users
-(
-    id       bigint auto_increment
-        primary key,
-    name     varchar(255) not null,
-    email    varchar(255) not null,
-    password varchar(255) not null
-);
+CREATE TABLE
+    `users` (
+                `id` bigint NOT NULL AUTO_INCREMENT,
+                `name` varchar(255) NOT NULL,
+                `email` varchar(255) NOT NULL,
+                `password` varchar(255) NOT NULL,
+                PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
 
-create table addresses
-(
-    id      bigint auto_increment
-        primary key,
-    city    varchar(255) not null,
-    zip     varchar(255) not null,
-    street  varchar(255) not null,
-    user_id bigint       not null,
-    constraint addresses_users_id_fk
-        foreign key (user_id) references users (id)
-);
+CREATE TABLE
+    `addresses` (
+                    `id` bigint NOT NULL AUTO_INCREMENT,
+                    `city` varchar(255) NOT NULL,
+                    `zip` varchar(255) NOT NULL,
+                    `street` varchar(255) NOT NULL,
+                    `user_id` bigint NOT NULL,
+                    PRIMARY KEY (`id`),
+                    KEY `addresses_users_id_fk` (`user_id`),
+                    CONSTRAINT `addresses_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
+
+CREATE TABLE
+    `profiles` (
+                   `id` bigint NOT NULL,
+                   `bio` text,
+                   `phone_number` varchar(15) DEFAULT NULL,
+                   `date_of_birth` date DEFAULT NULL,
+                   `loyalty_points` int unsigned DEFAULT '0',
+                   PRIMARY KEY (`id`),
+                   CONSTRAINT `profiles_relation_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE
+    `tags` (
+               `id` int unsigned NOT NULL AUTO_INCREMENT,
+               `name` varchar(255) DEFAULT NULL,
+               PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE
+    `user_tags` (
+                    `user_id` bigint NOT NULL,
+                    `tag_id` int unsigned NOT NULL,
+                    PRIMARY KEY (`user_id`, `tag_id`),
+                    KEY `user_tags_relation_2` (`tag_id`),
+                    CONSTRAINT `user_tags_relation_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+                    CONSTRAINT `user_tags_relation_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
