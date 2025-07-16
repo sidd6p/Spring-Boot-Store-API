2ddc5dd99beabc34a5b9b91dbe35d4e83b97d04f
Siddhartha <siddpurwar@gmail.com>
2025-07-16 16:13:56 +0530

    BUILD: response entity

--- src/main/java/com/github/sidd6p/store/controllers/UserController.java
+++ src/main/java/com/github/sidd6p/store/controllers/UserController.java
@@ -3,20 +3,32 @@ package com.github.sidd6p.store.controllers;
 import com.github.sidd6p.store.entities.User;
 import com.github.sidd6p.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/v1/users")
+    @GetMapping()
     public Iterable<User> getAllUsers() {
-        var users =  userRepository.findAll();
-        System.out.println(users);
-        return users;
+        return userRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // creating ResponseEntity object manually
+        } else {
+            return ResponseEntity.ok(user); // using ResponseEntity builder
+        }
+    }
+
 }
