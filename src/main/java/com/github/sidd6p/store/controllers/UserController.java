44983fc9cb3617caeb7dfbda9449f8eb531f659d
Siddhartha <siddpurwar@gmail.com>
2025-07-17 15:56:27 +0530

    BUILD: Sorting on users controller

--- src/main/java/com/github/sidd6p/store/controllers/UserController.java
+++ src/main/java/com/github/sidd6p/store/controllers/UserController.java
@@ -4,14 +4,13 @@ import com.github.sidd6p.store.dtos.UserDto;
 import com.github.sidd6p.store.mappers.UserMapper;
 import com.github.sidd6p.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 
 @RestController
@@ -22,14 +21,17 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping()
-    public List<UserDto> getAllUsers() {
+    public List<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sortBy) {
         // We use stream() to process the List<User> from userRepository in a functional way.
         // stream() converts the List into a Stream, which allows us to use map(), filter(), etc.
         // map() transforms each User object into a UserDto object.
         // toList() collects the results from the Stream back into a List.
         // This approach is useful for transforming or filtering data efficiently.
         // Original list -> Stream -> map() -> Stream -> toList() -> List result
-        return userRepository.findAll()
+        if (!Set.of("id", "name", "email").contains(sortBy)) {
+            sortBy = "id";
+        }
+        return userRepository.findAll(Sort.by(sortBy).ascending())
                 .stream()
                 .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
                 .toList(); // converts Stream<UserDto> to List<UserDto>
--- src/main/java/com/github/sidd6p/store/dtos/UserDto.java
+++ src/main/java/com/github/sidd6p/store/dtos/UserDto.java
@@ -1,12 +1,18 @@
 package com.github.sidd6p.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+
+    @JsonIgnore
     private long id;
+
+    @JsonProperty("user_name")
     private String name;
     private  String email;
 }
