383fce5aec05c57ee6ae2ee28ce4d20bb58cd84b
Siddhartha Purwar <siddpurwar@gmail.com>
2025-09-28 02:58:20 +0530

    FIX: cart price

--- src/main/java/com/github/sidd6p/store/mappers/CartMapper.java
+++ src/main/java/com/github/sidd6p/store/mappers/CartMapper.java
@@ -3,8 +3,10 @@ package com.github.sidd6p.store.mappers;
 import com.github.sidd6p.store.dtos.CartDto;
 import com.github.sidd6p.store.entities.Cart;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {CartItemMapper.class})
 public interface CartMapper {
+    @Mapping(target = "price", expression = "java(cart.getCartItems().stream().map(item -> item.getProduct().getPrice().multiply(java.math.BigDecimal.valueOf(item.getQuantity()))).reduce(java.math.BigDecimal.ZERO, java.math.BigDecimal::add))")
     CartDto toDto(Cart cart);
 }
